a287b0aecf9247efb322f1f8c366fd8f6a3b3407
mitsim1000 <mitsim1000@gmail.com>
2023-02-27 04:27:40 +0000

    Fahr2Cel

--- Module5/Fahr2Cel.s
+++ Module5/Fahr2Cel.s
@@ -1,28 +1,31 @@
 .text
 .global main
 main:
+    //Push stack
     SUB sp, sp, #4
     STR lr, [sp]
 
-    LDR r0, =prompt
-    BL printf
-    LDR r0, =formatString
-    LDR r1, =fahr
+    LDR r0, =prompt //Load prompt
+    BL printf //Print prompt
+    LDR r0, =formatString //Load format string
+    LDR r1, =fahr //Load integer
     BL scanf
 
     LDR r0, =fahr
-    LDR r0, [r0]
+    LDR r0, [r0] //Load fahr input value
     MOV r1, #32
-    SUB r0, r0, r1
+    SUB r0, r0, r1 //Subtract 32
     MOV r1, #5
-    MUL r0, r0, r1
+    MUL r0, r0, r1 //Multiply by 5
     MOV r1, #9
-    BL __aeabi_idiv
+    BL __aeabi_idiv //Divide by 9
     
+    //Print output
     MOV r1, r0
     LDR r0, =output
     BL printf
 
+    //Pop stack
     LDR lr, [sp]
     ADD sp, sp, #4
     MOV pc,lr
