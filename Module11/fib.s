9261bc8335fce98faee8c27a1d3206cc948bbddf
mitsim1000 <mitsim1000@gmail.com>
2023-04-17 03:27:27 +0000

    fib.s

---
+++ Module11/fib.s
@@ -0,0 +1,25 @@
+# Program: fib.s
+# Author: Mitchell Simmons
+# Date 04/16/2023
+# Purpose: Program that calculates Fibonacci numbers recursively
+
+.global main
+
+.text
+main:
+    #Push stack
+    SUB sp, sp, #4
+    STR lr, [sp, #0]
+
+
+
+    #Pop stack
+    LDR lr, [sp, #0]
+    ADD sp, sp, #4
+    MOV pc, lr
+
+.data
+    prompt: .asciz "Enter a positive integer n to calculate the n'th Fibonacci number: "
+    format: .asciz "%d"
+    int: .word 0
+    output: .asciz "Corresponding Fibonacci number: %d"
